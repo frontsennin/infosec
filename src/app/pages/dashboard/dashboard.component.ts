f23225ac2bf035b7d175ef070946fb87c582aa5d
FrontSennin <nicolas.mazia@somosasoma.com.br>
2023-07-19 13:20:39 -0300

    feat: dashboard-page

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CardType } from 'src/app/shared/enums/card-type.enum';
+import { DashboardCardPosition } from 'src/app/shared/enums/dashboard-card-position.enum';
+import { dashboardMock } from 'src/app/shared/mocks/dashboard-mock';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,6 +9,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+
+  listOfCards = dashboardMock
+  dashboardCardPosition = DashboardCardPosition
+  cardType = CardType
 
   constructor() { }
 
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { ChartCardModule } from 'src/app/shared/layout/chart-card/chart-card.module';
+import { ListCardModule } from 'src/app/shared/layout/list-card/list-card.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    ChartCardModule,
+    ListCardModule
   ],
   declarations: [DashboardComponent]
 })
