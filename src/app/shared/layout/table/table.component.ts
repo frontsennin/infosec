cbe496c3509654c00ab1f47541c02206e8b29c9d
FrontSennin <nicolas.mazia@somosasoma.com.br>
2023-07-20 09:43:20 -0300

    feat: table shared component ts

--- src/app/shared/layout/table/table.component.ts
+++ src/app/shared/layout/table/table.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Table } from '../../models/table';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
+
+  @Input() tableConfig!:Table
+  @Output() bodyEmitter = new EventEmitter<any>()
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  emitValue(body:any) {
+    this.bodyEmitter.emit(body)
   }
 
 }
