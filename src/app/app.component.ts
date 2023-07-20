9b5763e4b835707237b5f8adc83f603879fbdaf1
FrontSennin <nicolas.mazia@somosasoma.com.br>
2023-07-20 10:14:13 -0300

    feat: dark-mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { listOfPages } from './shared/routes/pages';
+import { DarkModeService } from 'angular-dark-mode';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title?:string;
   pageIcon:any
 
   constructor(
-    private router: Router
+    private router: Router,
+    private darkModeService: DarkModeService
   ) {
     router.events.subscribe(() => {
       let menu
@@ -25,5 +27,9 @@ export class AppComponent {
       this.title = menu?.pageTitle
       this.pageIcon = menu?.icon
     })
+  }
+
+  ngOnInit() {
+    this.darkModeService.darkMode$;
   }
 }
--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { faGear, faNetworkWired, faShieldHalved, faUser } from '@fortawesome/free-solid-svg-icons';
+import { DarkModeService } from 'angular-dark-mode';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { faGear, faMoon, faNetworkWired, faShieldHalved, faSun, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -8,14 +9,28 @@ import { faGear, faNetworkWired, faShieldHalved, faUser } from '@fortawesome/fre
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() emitDarkMode = new EventEmitter<any>()
+
   faUser = faUser
   faGear = faGear
+  faSun = faSun
+  faMoon = faMoon
   faShield = faShieldHalved
   faNetwork = faNetworkWired
 
-  constructor() { }
+  isLight = false
+
+  constructor(
+    private darkModeService: DarkModeService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onToggle(): void {
+    this.isLight = !this.isLight;
+    this.darkModeService.toggle();
+    this.emitDarkMode.emit(this.isLight);
   }
 
 }
